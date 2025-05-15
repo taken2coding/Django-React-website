72f701a103effb7ec03e129e81f3144382dbc376
Peter Ugwuoke <sirp4all@gmail.com>
2025-05-15 13:45:54 +0100

    added production vieww

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -4,10 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
