a24ad6420e4f4186787c29675418467ebb776ecb
Peter Ugwuoke <sirp4all@gmail.com>
2025-05-15 13:25:59 +0100

    Updated Vite-config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://django-react-website.onrender.com',
         changeOrigin: true,
       },
     },
