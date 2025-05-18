1ddf622f5030b8e6630d435671cbc675a68b7379
Peter Ugwuoke <sirp4all@gmail.com>
2025-05-18 19:06:01 +0100

    Frontend and backend fully integrated phase 6

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
